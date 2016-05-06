0628576eb4fcac3c5fc2f52bdae687675cf2a43c
Rosanne Combremont <rosanne.combremont@heig-vd.ch>
2016-05-06 16:51:50 +0200

    data base done

---
+++ dataBase/BDD_script.sql
@@ -0,0 +1,25 @@
+DROP SCHEMA IF EXISTS Yahtzee;
+CREATE schema Yahtzee;
+USE Yahtzee;
+
+CREATE TABLE Joueur(
+	ID int(10) AUTO_INCREMENT,
+    Username varchar(20),
+    MDP varchar(40),
+    ScoreTotal int(15),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Partie(
+	ID int(15) AUTO_INCREMENT,
+    Etat varchar(15),    
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Partie_Joueur(
+	ID_joueur int(10),
+    ID_partie int(15)
+);
+
+ALTER TABLE Partie_Joueur ADD FOREIGN KEY (ID_joueur) REFERENCES Joueur (ID);
+ALTER TABLE Partie_Joueur ADD FOREIGN KEY (ID_partie) REFERENCES Partie (ID);
