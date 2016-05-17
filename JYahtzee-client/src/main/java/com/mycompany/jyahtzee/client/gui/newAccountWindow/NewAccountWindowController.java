14ecbdf7ff9ca32f58602fe9bbb7c3be86c50179
Madolyne Dupraz <madolyne.dupraz@heig-vd.ch>
2016-05-17 22:01:29 +0200

    GUI

--- JYahtzee-client/src/main/java/com/mycompany/jyahtzee/client/gui/newAccountWindow/NewAccountWindowController.java
+++ JYahtzee-client/src/main/java/com/mycompany/jyahtzee/client/gui/newAccountWindow/NewAccountWindowController.java
@@ -1,6 +1,8 @@
 package com.mycompany.jyahtzee.client.gui.newAccountWindow;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -11,10 +13,22 @@ public class NewAccountWindowController {
 
    @FXML
    private Pane mainPane;
+    @FXML
+    private TextField login;
+    @FXML
+    private PasswordField password;
+
 
     @FXML
     private void cancel()
     {
         ((Stage)mainPane.getScene().getWindow()).close();
+    }
+
+    // bien entendu nom a changer...exemple pour Fafa
+    @FXML
+    private void blabla() {
+        login.getText();
+        password.getText();
     }
 }
