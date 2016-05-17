12947ed593c88da3dee4764ee22e87c3306d88cc
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-05-17 14:07:40 +0200

    [REFACT] Factorisation du code + ajout de commentaires

--- JYahtzee-client/src/main/java/com/mycompany/jyahtzee/client/transport/Client.java
+++ JYahtzee-client/src/main/java/com/mycompany/jyahtzee/client/transport/Client.java
@@ -15,78 +15,99 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
-    
-    private String serverHost;
-    private int serverPort;
+
+    private final String serverHost;
+    private final int serverPort;
     private boolean connected = false;
     private Socket clientSocket;
     private BufferedReader reader;
     private PrintWriter writer;
 
+    /**
+     * Construit un nouveau client en spécifiant l'hôte et le port du serveur.
+     *
+     * @param serverHost
+     * @param serverPort
+     */
     public Client(String serverHost, int serverPort) {
         this.serverHost = serverHost;
         this.serverPort = serverPort;
     }
-    
-    public void connect() {
+
+    /**
+     * Méthode appelé lors de la connexion d'un client.
+     *
+     * @throws IOException
+     */
+    public void connect() throws IOException {
         try {
             clientSocket = new Socket(serverHost, serverPort);
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             writer = new PrintWriter(clientSocket.getOutputStream());
-            
-            connected = true;
-            LOG.info(reader.readLine());
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             cleanup();
         }
-    }
 
-    public void disconnect() {
+        connected = true;
+        LOG.info(reader.readLine());
+    }
+    
+    /**
+     * Méthode permettant de fermer tout les flux ouverts. Elle est généralement
+     * appelée en cas de fermeture de la connexion.
+     * 
+     * @throws java.io.IOException
+     */
+    public void cleanup() throws IOException {
+        if (reader != null) {
+            reader.close();
+        }
+        if (writer != null) {
+            writer.close();
+        }
+        if (clientSocket != null) {
+            clientSocket.close();
+        }
+    }
+    
+    /**
+     * Cette méthode est appelé lorsque qu'un client ce déconnecte du serveur.
+     * 
+     * @throws java.io.IOException
+     */
+    public void disconnect() throws IOException {
         connected = false;
         cleanup();
     }
 
-    public void cleanup() {
-        if (reader != null) {
-            try {
-                reader.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-        if (writer != null) {
-            writer.close();
-        }
-        
-        if (clientSocket != null) {
-            try {
-                clientSocket.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-    
-    public void sendMessage(String msg) {                
+    /**
+     * Méthode utilitaire permettant d'envoyer un message au serveur.
+     * 
+     * @param msg
+     * @throws IOException 
+     */
+    public void sendMessage(String msg) throws IOException {
         writer.write(msg);
         writer.flush();
-        if(msg.equals(Protocole.CMD_BYE))
-        {
+
+        if (msg.equals(Protocole.CMD_BYE)) {
             disconnect();
         }
     }
-    public String receiveMessage()
-    {
+
+    /**
+     * Méthode utilitaire permettant de recevoir un message de la part du serveur.
+     * 
+     * @return 
+     */
+    public String receiveMessage() {
         String msg;
-        try
-        {
+        try {
             msg = reader.readLine();
+            
             return msg;
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return e.toString();
         }
     }
