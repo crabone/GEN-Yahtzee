f079754ea7075e06956fa2058f064495eec4a705
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-05-17 14:57:58 +0200

    [UPDATE] Préparation/Simplification de la fonction d'authenfication coté serveur pour le client

--- JYahtzee-server/src/main/java/com/mycompany/jyahtzee/server/transport/MultiThreadedServer.java
+++ JYahtzee-server/src/main/java/com/mycompany/jyahtzee/server/transport/MultiThreadedServer.java
@@ -10,52 +10,48 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author crab_one
- */
 public class MultiThreadedServer {
 
     private static final Logger LOG = Logger.getLogger(MultiThreadedServer.class.getName());
-    
+
     final int port;
     boolean connected;
 
     /**
      * Construit un serveur multithreadé, en spécifiant le port d'écoute.
-     * 
+     *
      * @param port
      */
     public MultiThreadedServer(int port) {
         this.port = port;
         connected = false;
     }
-    
+
     /**
-     * Cette méthode est appelé après construction de l'objet, servant à 
+     * Cette méthode est appelé après construction de l'objet, servant à
      * recevoir les clients.
      */
-    public void serveClients() {        
+    public void serveClients() {
         new Thread(new ReceptionistWorker()).start();
     }
-    
+
     /**
-     * Cette classe est chargé de réceptionner les clients qui arrivent sur
-     * le serveur.
+     * Cette classe est chargé de réceptionner les clients qui arrivent sur le
+     * serveur.
      */
     private class ReceptionistWorker implements Runnable {
 
         @Override
         public void run() {
             ServerSocket serverSocket;
-            
+
             try {
                 serverSocket = new ServerSocket(port);
             } catch (IOException ex) {
                 Logger.getLogger(MultiThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
                 return;
             }
-            
+
             while (true) {
                 LOG.log(Level.INFO, "Waiting (blocking) for a new client on port {0}", port);
                 try {
@@ -64,17 +60,17 @@ public class MultiThreadedServer {
                 } catch (IOException ex) {
                     Logger.getLogger(MultiThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            }            
+            }
         }
-        
+
     }
-    
+
     /**
      * Cette classe est chargée de servir les clients après qu'ils aient été
      * reçu.
      */
     private class ServantWorker implements Runnable {
-        
+
         Socket clientSocket;
         BufferedReader reader = null;
         PrintWriter writer = null;
@@ -87,7 +83,7 @@ public class MultiThreadedServer {
             } catch (IOException ex) {
                 Logger.getLogger(MultiThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
             connected = true;
         }
 
@@ -97,35 +93,28 @@ public class MultiThreadedServer {
             boolean shouldRun = true;
             boolean ok;
             GameManager gameManager = new GameManager();
-            
-            //writer.println("Welcome to our Yahtzee server.");
-            //writer.flush();
-            
+
             try {
                 while ((shouldRun == true) && (line = reader.readLine()) != null) {
-                    
-                    switch(line)
-                    {
+
+                    switch (line) {
                         case (Protocole.CMD_HI):
                             writer.println(Protocole.CMD_HI);
                             writer.flush();
                             break;
                         case (Protocole.CMD_AUTH):
-                            auth();
+                            authenticate();
                             break;
                         case (Protocole.CMD_INSCRIPTION):
-                            inscription();
+                            register();
                             break;
                         case (Protocole.CMD_CREATION):
                             ok = gameManager.createGame();
-                            if(ok)
-                            {
-                                    writer.println(Protocole.CMD_OK);
+                            if (ok) {
+                                writer.println(Protocole.CMD_OK);
+                            } else {
+                                writer.println(Protocole.CMD_KO);
                             }
-                            else
-                            {
-                                    writer.println(Protocole.CMD_KO);
-                            }                		
                             writer.flush();
                             break;
                         case (Protocole.CMD_JOIN):
@@ -133,20 +122,16 @@ public class MultiThreadedServer {
                             writer.println(Protocole.CMD_ACK);
                             writer.flush();
                             id = Integer.parseInt(reader.readLine());
-                            if(id == 0)
-                            {
-                                    writer.println(Protocole.CMD_KO);
-                                    writer.flush();
-                                    break;
+                            if (id == 0) {
+                                writer.println(Protocole.CMD_KO);
+                                writer.flush();
+                                break;
                             }
-                            ok = gameManager.joinGame(id,player);
-                            if(ok)
-                            {
-                                    writer.println(Protocole.CMD_OK);
-                            }
-                            else
-                            {
-                                    writer.println(Protocole.CMD_KO);
+                            ok = gameManager.joinGame(id, player);
+                            if (ok) {
+                                writer.println(Protocole.CMD_OK);
+                            } else {
+                                writer.println(Protocole.CMD_KO);
                             }
                             writer.flush();
                             break;
@@ -162,15 +147,12 @@ public class MultiThreadedServer {
                                     writer.flush();
                                     break;      
                             }
-                            */
+                             */
                             ok = gameManager.observeGame(idGame);
-                            if(ok)
-                            {
-                                    writer.println(Protocole.CMD_OK);
-                            }
-                            else
-                            {
-                                    writer.println(Protocole.CMD_KO);
+                            if (ok) {
+                                writer.println(Protocole.CMD_OK);
+                            } else {
+                                writer.println(Protocole.CMD_KO);
                             }
                             writer.flush();
                             break;
@@ -182,11 +164,10 @@ public class MultiThreadedServer {
                             writer.println(Protocole.CMD_ACK);
                             writer.flush();
                             idScore = reader.readLine();
-                            if(idScore == null)
-                            {
-                                    writer.println(Protocole.CMD_KO);
-                                    writer.flush();
-                                    break;
+                            if (idScore == null) {
+                                writer.println(Protocole.CMD_KO);
+                                writer.flush();
+                                break;
                             }
                             /*
                             //ok = fonction_enregistrer_score(idScore);
@@ -198,7 +179,7 @@ public class MultiThreadedServer {
                             {
                                     writer.println(Protocole.CMD_KO);
                             }
-                            */
+                             */
                             writer.flush();
                             break;
                         case (Protocole.CMD_BYE):
@@ -211,7 +192,7 @@ public class MultiThreadedServer {
                             break;
                     }
                 }
-                
+
                 disconnect();
             } catch (IOException ex) {
                 try {
@@ -219,15 +200,14 @@ public class MultiThreadedServer {
                 } catch (IOException ex1) {
                     Logger.getLogger(MultiThreadedServer.class.getName()).log(Level.SEVERE, null, ex1);
                 }
-            }            
+            }
         }
-        
+
         /**
-         * Méthode permettant de fermer tout les flux ouverts. Elle est généralement
-         * appelée en cas de fermeture de la connexion.
+         * Méthode permettant de fermer tout les flux ouverts. Elle est
+         * généralement appelée en cas de fermeture de la connexion.
          */
-        private void cleanup() throws IOException
-        {
+        private void cleanup() throws IOException {
             if (reader != null) {
                 reader.close();
             }
@@ -238,100 +218,105 @@ public class MultiThreadedServer {
                 clientSocket.close();
             }
         }
-        
+
         /**
-         * Cette méthode est appelé lorsque qu'un client ce déconnecte du serveur.
+         * Cette méthode est appelé lorsque qu'un client ce déconnecte du
+         * serveur.
          */
-        private void disconnect() throws IOException
-        {
+        private void disconnect() throws IOException {
             connected = false;
             cleanup();
         }
-        
-        private void auth() throws IOException
-        {
+
+        private void authenticate() throws IOException {
             String mdp;
             String username;
             String line;
 
             writer.println(Protocole.CMD_ACK);
             writer.flush();
-            while((line = reader.readLine()).equals(Protocole.CMD_USERNAME))
-            {
-        	writer.println("Not a correct command");
-            	writer.flush();
-            }
-            writer.println(Protocole.CMD_ACK);
-            writer.flush();
-            username = reader.readLine();
-            if(username == null)
-            {
-            	writer.println(Protocole.CMD_KO);
-            	writer.flush();
-            	return;
-            }
-            writer.println(Protocole.CMD_OK);
-            writer.flush();
-            while((line = reader.readLine()).equals(Protocole.CMD_MDP))
-            {
+            while ((line = reader.readLine()).equals(Protocole.CMD_USERNAME)) {
                 writer.println("Not a correct command");
-            	writer.flush();
-            }
-            writer.println(Protocole.CMD_ACK);
-            writer.flush();
-            mdp = reader.readLine();
-            if(mdp == null)
-            {
-                writer.println(Protocole.CMD_KO);
-        	writer.flush();
-        	return;
-            }
-            writer.println(Protocole.CMD_OK);
-            writer.flush();
-        }
-        
-        private void inscription() throws IOException
-        {
-            String mdp;
-            String username;
-            String line;
-
-            writer.println(Protocole.CMD_ACK);
-            writer.flush();
-            while((line = reader.readLine()).equals(Protocole.CMD_USERNAME))
-            {
-        	writer.println("Not a correct command");
-            	writer.flush();
+                writer.flush();
             }
             writer.println(Protocole.CMD_ACK);
             writer.flush();
             username = reader.readLine();
-            if(username == null)
-            {
+            if (username == null) {
                 writer.println(Protocole.CMD_KO);
-        	writer.flush();
+                writer.flush();
                 return;
             }
             writer.println(Protocole.CMD_OK);
             writer.flush();
-            while((line = reader.readLine()).equals(Protocole.CMD_MDP))
-            {
-            	writer.println("Not a correct command");
-            	writer.flush();
+            while ((line = reader.readLine()).equals(Protocole.CMD_MDP)) {
+                writer.println("Not a correct command");
+                writer.flush();
             }
             writer.println(Protocole.CMD_ACK);
             writer.flush();
             mdp = reader.readLine();
-            if(mdp == null)
-            {
+            if (mdp == null) {
                 writer.println(Protocole.CMD_KO);
-        	writer.flush();
-        	return;
+                writer.flush();
+                return;
             }
             writer.println(Protocole.CMD_OK);
             writer.flush();
-            //fonction_inscription_BDD(username, mdp); 
         }
-        
+
+        private void register() throws IOException {
+            String mdp;
+            String username;
+            String line;
+
+            writer.println(Protocole.CMD_ACK);
+            writer.flush();
+            line = reader.readLine();
+            
+            if (!line.equals(Protocole.CMD_USERNAME)) {
+                // On balance une erreure et on quitte la fonction. Ca signifie
+                // que le client tente quelque chose de suspect. Afin de garantir
+                // l'intégrité des données on quitte tout de suite.
+                writer.println(Protocole.CMD_KO);
+                writer.flush();
+                return;
+            }
+            else {
+                writer.println(Protocole.CMD_ACK);
+                writer.flush();
+            }
+
+            username = reader.readLine();
+            if (username == null) {
+                writer.println(Protocole.CMD_KO);
+                writer.flush();
+                return;
+            }
+            // A faire, vérifier que le nom d'utilisateur n'existe pas sinon erreure.
+            else {
+                writer.println(Protocole.CMD_OK);
+                writer.flush();                
+            }
+            
+            /* On oublie pour le moment le mot de passe pour vérifier que les fonctions fonctionnent
+            while ((line = reader.readLine()).equals(Protocole.CMD_MDP)) {
+                writer.println("Not a correct command");
+                writer.flush();
+            }
+            writer.println(Protocole.CMD_ACK);
+            writer.flush();
+            mdp = reader.readLine();
+            if (mdp == null) {
+                writer.println(Protocole.CMD_KO);
+                writer.flush();
+                return;
+            }
+            writer.println(Protocole.CMD_OK);
+            writer.flush();
+            */
+            //fonction_register_BDD(username, mdp); 
+        }
+
     }
 }
