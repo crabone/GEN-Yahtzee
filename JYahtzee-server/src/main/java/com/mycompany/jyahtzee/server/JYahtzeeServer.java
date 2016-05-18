51c8f073b349a4402dabd734e907a1883c6c74d4
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-05-18 17:11:09 +0200

    [UPDATE] Ajout des fonctions d'auth et inscription

--- JYahtzee-server/src/main/java/com/mycompany/jyahtzee/server/JYahtzeeServer.java
+++ JYahtzee-server/src/main/java/com/mycompany/jyahtzee/server/JYahtzeeServer.java
@@ -3,10 +3,7 @@ package com.mycompany.jyahtzee.server;
 import com.mycompany.jyahtzee.server.transport.MultiThreadedServer;
 import java.io.IOException;
 
-/**
- *
- * @author crab_one
- */
+
 public class JYahtzeeServer {
     public static void main(String[] args) throws IOException {
         MultiThreadedServer server = new MultiThreadedServer(4321);
